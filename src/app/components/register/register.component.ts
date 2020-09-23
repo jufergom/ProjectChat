8d1cbee1f2e607b15d2a15b2e922c7339fe6402f
jufergom <jufergom@hotmail.com>
2020-09-23 04:28:13 -0600

    register component is now connected to backend

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl("",Validators.required),
+      username: new FormControl("",Validators.required),
+      password: new FormControl("",Validators.required),
+      confirmPassword: new FormControl("",Validators.required)
+    })
   }
+
+  onSubmit(): void {
+    let formValue = this.form.value;
+    if(formValue.password == formValue.confirmPassword) {
+      let user: User = {
+        name: formValue.name,
+        username: formValue.username,
+        password: formValue.password
+      };
+      this.userService.addUser(user).subscribe(res => {
+        if(formValue)
+        alert(`Created new user ${user.username}`);
+      }, err => {
+        alert(`User ${user.username} already exist`);
+      });
+    }
+    else {
+      alert("The passwords you entered did not match");
+    }
+  }
+
 
 }
